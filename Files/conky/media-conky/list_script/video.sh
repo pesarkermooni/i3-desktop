80763122847e7efb5eb659b9dfd4253e22128cae
rasoolsn <rasoolentertainment@openmailbox.org>
2016-06-19 22:20:15 +0430

    Edit list_script

--- Files/conky/media-conky/list_script/video.sh
+++ Files/conky/media-conky/list_script/video.sh
@@ -6,11 +6,10 @@ DL_ADDRESS="$HOME/Downloads"
 VIDEO_FORMAT="*mp4 *mkv *avi *mpg *mov *flv"
 TMP_LIST="/tmp/videolist"
 NULL_ADDRESS="/dev/null"
-CONKY_ADDRESS="$HOME/.i3/Files/conky/media-conky/kill_script"
 NUMBER="1"
 
 while [ "1" = "1" ]; do
-	$CONKY_ADDRESS/kill_stop-cont_script.sh stop
+	touch "${TMP_LIST}1"
 	cd "$HOME_ADDRESS"
 	ls -1t $VIDEO_FORMAT > "$TMP_LIST" 2> "$NULL_ADDRESS"
 	cd "$DESK_ADDRESS"
@@ -18,7 +17,7 @@ while [ "1" = "1" ]; do
 	cd "$DL_ADDRESS"
 	ls -1t $VIDEO_FORMAT >> "$TMP_LIST" 2> "$NULL_ADDRESS"
 	if [[ $(cat "$TMP_LIST") = "" ]];then
-			rm -f "$TMP_LIST"
+			rm -f "${TMP_LIST}"*
 		else
 			while [ "$NUMBER" != "11" ];do
 				echo >> "$TMP_LIST"
@@ -26,6 +25,6 @@ while [ "1" = "1" ]; do
 			done
 			NUMBER="1"
 	fi
-	$CONKY_ADDRESS/kill_stop-cont_script.sh cont
+	rm -f "${TMP_LIST}1"
 	sleep 30s
 done
